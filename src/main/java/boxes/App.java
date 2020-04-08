cb7a8b316a27d05506671542c36dadd733980acb
mohamed mhiri <mhiri.mohamed.5@gmail.com>
2020-04-08 21:27:23 +0100

    initial java gradle app

---
+++ src/main/java/boxes/App.java
@@ -0,0 +1,8 @@
+package boxes;
+
+public class App {
+	public static void main(String[] args) {
+		Box box = new Box();
+		System.out.println(box.toString());
+	}
+}
---
+++ src/main/java/boxes/Box.java
@@ -0,0 +1,9 @@
+package boxes;
+
+public class Box {
+	public int height;
+	public int width;
+	public String toString() {
+		return "Box: \nheight: " + this.height + "\nwidth: "+ this.width;
+	}
+}
